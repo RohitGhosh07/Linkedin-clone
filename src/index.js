a4046442acefa28bd465e85283fcd4770d5995c2
ionicthor <ioniccompound6@gmail.com>
2023-08-20 23:15:17 +0530

    initial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
+// Internal Imports
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// External Imports
+import { BrowserRouter } from "react-router-dom";
+
+// Styles
 import './index.css';
+
+// Components
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
